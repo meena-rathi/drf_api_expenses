832764118595d3750cdadfc0ee1b60c8452e7584
Meena Kumari <meenakumari16@hotmail.com>
2024-09-05 18:45:14 +0000

    add profile

--- profiles/models.py
+++ profiles/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from cloudinary.models import CloudinaryField
 
 class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='images/', default='../default_profile_qdjgyp')
-
+    # image = models.ImageField(upload_to='image/', default='../default_profile_qdjgyp')
+    image = CloudinaryField(
+        'image',
+        transformation={
+            'crop': 'limit',
+            'width': 800,
+            'height': 800
+        },
+        default='../placeholder_profile_xnpcwj.webp'
+    )
     def __str__(self):
         return f"{self.owner}'s profile"
 
